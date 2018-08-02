92b7839cc8a51eaed0bada3ddc97995f47973063
ErikaBraithe <erikabraithwaite@gmail.com>
2018-08-02 12:14:53 -0400

    add R and Rmd files

---
+++ PODS-plotly-example.R
@@ -0,0 +1,42 @@
+devtools::install_github('hadley/ggplot2')
+pacman::p_load(plotly, dplyr)
+
+
+# Load data#
+data(txhousing)
+
+# Select variables, subset by city, omit missing variables, convert to data frame
+tx = txhousing %>%
+        select(city, month, median, year) %>%
+        filter(city %in% c('Galveston', 'Bay Area', 'Port Arthur', 'Austin')) %>% 
+        group_by(city, year) %>% 
+        summarise(avg = mean(median)) %>% 
+        na.omit() %>% 
+        data.frame() 
+
+# Explore data
+nrow(tx)
+head(tx)
+
+
+# Static ggplot of housing prices by month, stratified by city 
+texas_plot = ggplot(tx, aes(x = year, y = avg, color = city), factor = city) + 
+        geom_path() +
+        labs(y = 'Average price ($USD)', 
+             title = 'Texas housing prices 2000-2015, by month') + 
+        theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+        scale_x_continuous(breaks = pretty(tx$year, n = 15))
+
+texas_plot
+gg = ggplotly(texas_plot)
+
+
+# Using plotly
+
+pplot = plot_ly(data = tx, x = ~year, y = ~avg) %>% 
+        add_lines(color = ~city) %>% 
+        rangeslider() %>% 
+        layout(xaxis = list(title = ""))
+pplot
+
+
